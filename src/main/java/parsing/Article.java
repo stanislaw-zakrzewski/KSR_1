e48ad47a1081bd9fb5e7597b6c0da387822aa8bb
Stanisław Zakrzewski <sabrisv123@gmail.com>
2019-03-31 13:43:12 +0200

    Article class

--- src/main/java/parsing/Article.java
+++ src/main/java/parsing/Article.java
@@ -1,5 +1,27 @@
 package parsing;
 
-public class Article {
+import java.util.List;
 
+public class Article {
+    private List<String> tags;
+    private String title;
+    private String content;
+
+    public Article(List<String> tags, String title, String content) {
+        this.tags = tags;
+        this.title = title;
+        this.content = content;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
 }
