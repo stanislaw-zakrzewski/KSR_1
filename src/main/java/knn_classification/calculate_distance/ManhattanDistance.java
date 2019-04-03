4bb2f72d0d98e12125a10ff1b1d28345df02488a
Florke07 <maciek.socha1@gmail.com>
2019-04-03 19:16:49 +0200

    Implemented ManhattanDistance

--- src/main/java/knn_classification/calculate_distance/ManhattanDistance.java
+++ src/main/java/knn_classification/calculate_distance/ManhattanDistance.java
@@ -3,9 +3,12 @@ package knn_classification.calculate_distance;
 import java.util.List;
 
 public class ManhattanDistance implements Distance {
-    //TODO implement
     @Override
     public float calculate(List<Float> vector1, List<Float> vector2) {
-        return 0;
+        float sum = 0;
+        for (int i=0;i<vector1.size();i++) {
+            sum += Math.abs(vector1.get(i) - vector2.get(i));
+        }
+        return sum;
     }
 }
