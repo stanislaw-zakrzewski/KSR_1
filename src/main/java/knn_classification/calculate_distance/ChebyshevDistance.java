0394bb2115dc4c7d2fc58d485ba7018417cc08ff
Florke07 <maciek.socha1@gmail.com>
2019-04-04 19:19:07 +0200

    ChebyshevDistance

--- src/main/java/knn_classification/calculate_distance/ChebyshevDistance.java
+++ src/main/java/knn_classification/calculate_distance/ChebyshevDistance.java
@@ -3,10 +3,12 @@ package knn_classification.calculate_distance;
 import java.util.List;
 
 public class ChebyshevDistance implements Distance {
-
-    //TODO implement chebyshev distance
     @Override
     public float calculate(List<Float> vector1, List<Float> vector2) {
-        return 0;
+        float mx = Math.abs(vector1.get(0) - vector2.get(0));
+        for (int i=0;i<vector1.size();i++) {
+            if (Math.abs(vector1.get(i) - vector2.get(i)) > mx) mx = Math.abs(vector1.get(i) - vector2.get(i));
+        }
+        return mx;
     }
 }
