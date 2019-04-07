2e742a0ba4b592e74e6ce90fdce0d2f1d8fb92d7
Florke07 <maciek.socha1@gmail.com>
2019-04-07 17:50:02 +0200

    NiGrams!

--- src/main/java/matching_words/word_comparators/NGrams.java
+++ src/main/java/matching_words/word_comparators/NGrams.java
@@ -1,32 +1,26 @@
 package matching_words.word_comparators;
 
-public class NGrams implements WordComparator{
-
-    //Uogulniona miara NGranow
+public class NGrams implements WordComparator {
+    //TODO change name of this class and implement it
 
     @Override
     public float similarity(Object o1, Object o2) {
+        int N = 0;
         String s1 = (String)o1;
         String s2 = (String)o2;
         s1 = s1.toLowerCase();
         s2 = s2.toLowerCase();
-        int N = 0;
-        int h = 0;
 
         if (s1.length() > s2.length()) N = s1.length();
         else N = s2.length();
 
-        for (int i=1;i<=s1.length();i++) {
-            for (int j=0;j<(s1.length()-i+1);j++) {
-                if (s2.contains((s1.substring(j, j+i)))) h++;
-            }
+        int n = 3;
+        int h = 0;
+
+        for (int i=0;i<s1.length()-n;i++) {
+            if (s2.contains((s1.substring(i, i+n)))) h++;
         }
 
-        //TODO think about this
-        if((h * (2f/((N*N)+N))) < .5) {
-            return 0;
-        }
-
-        return (h * (2f/((N*N)+N)));
+        return ((float)h/((float)N-(float)n+1));
     }
 }
